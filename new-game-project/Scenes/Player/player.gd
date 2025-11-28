e98e620dde57213b7619add57bbec6b3c0ed1d7c
Kelly <KZ.kellyzhang123@gmail.com>
2025-11-28 00:36:50 -0500

    imported new walk cycle

--- new-game-project/Scenes/Player/player.gd
+++ new-game-project/Scenes/Player/player.gd
@@ -8,11 +8,11 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_pressed("Left"):
 		x_direction = -1
-		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("walk")
 	elif Input.is_action_pressed("Right"):
 		x_direction = 1
-		$AnimatedSprite2D.flip_h = false
+		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("walk")
 	else:
 		x_direction = 0
