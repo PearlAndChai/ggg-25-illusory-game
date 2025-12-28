461396c695a5b04453ea9e9d26a783e9bcf10923
Theophylaktos <91230260+PearlAndChai@users.noreply.github.com>
2025-12-28 02:58:29 -0500

    Added house cg dialogue

---
+++ new-game-project/scenes/house_cg/label.gd
@@ -0,0 +1,16 @@
+extends Label
+
+const dialogue = [
+"Lucy: Huh…? What was that?",
+"Lucy: Is that the cat from earlier? What’s it doing here?",
+"Lucy: Wait, come back!",
+"Lucy: Wait, come back!",
+"Lucy: Wait, come back!",
+"Lucy: What… is this place? Was this the cat’s doing?",
+"Lucy: How did I get here? Can the cat take me back?"
+]
+
+func _ready():
+	for i in dialogue.size():
+		text = dialogue[i]
+		await get_tree().create_timer(2.0).timeout
